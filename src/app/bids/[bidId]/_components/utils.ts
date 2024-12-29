309c71f777c591eee398137d71561d237d62cf8a
9-max-6 <mutukumaxwel@gmail.com>
2024-12-29 08:50:21 +0300

    Fix: fixed an error that was causign the wrong last time since edit to be displayed.

--- src/app/bids/[bidId]/_components/utils.ts
+++ src/app/bids/[bidId]/_components/utils.ts
@@ -1,13 +1,14 @@
 import { differenceInCalendarDays } from "date-fns";
 export function getTime(updatedAt: Date | null, createdAt: Date) {
     if (!updatedAt) {
+        console.log("Running")
         const diff = differenceInCalendarDays(new Date(), createdAt)
         if (diff === 0) return "Last updated today"
         if (diff === 1) return "Last updated yesterday"
 
         return `Last updated ${diff} days ago`
     }
-    const diff = differenceInCalendarDays(new Date(), createdAt)
+    const diff = differenceInCalendarDays(new Date(), updatedAt)
     if (diff === 0) return "Last updated today"
     if (diff === 1) return "Last updated yesterday"
 
